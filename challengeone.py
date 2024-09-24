d3d6ec11372068e40d3190e7f884ee8b41df4209
StefCar1 <stefancarlson1991@gmail.com>
2024-09-24 20:21:12 +0000

    Join and split

---
+++ challengeone.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+print("Hello there!")
+
+name = input("What's your name?")
+
+print("I like that!")
+
+day = input("What day of the week is it?")
+
+print(f"Hello, {name}! Happy {day}!")
