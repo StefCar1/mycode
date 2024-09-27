a9872356ab2547cea7471094c6f0439b31908d34
StefCar1 <stefancarlson1991@gmail.com>
2024-09-27 21:55:46 +0000

    reading and writing to files

---
+++ dracula.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+count= 0
+with open("dracula.txt","r") as foo:
+    for line in foo:
+        if "vampire" in line:
+            print(line.lower())
+        if "vampire" in line.lower():
+            count += 1
+            print(count)
+
+counter= 0
+
+with open("dracula.txt","r") as foo:
+    with open("vampytimex.txt","w") as fang:
+        for line in foo:
+            if "vampire" in line.lower():
+                print(line)
+                counter += 1
+                fang.write(line)
+
+print(counter)
+
